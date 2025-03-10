6c43442c2466429fa7dc40fb89d206b5c0fe131f
Renan G Dutra <r.granatodutra@gmail.com>
2025-03-09 23:10:45 -0300

    feat: add input component

---
+++ src/lib/components/input.tsx
@@ -0,0 +1,62 @@
+"use client";
+import React, { useId, useRef } from "react";
+import { FaXmark } from "react-icons/fa6";
+
+interface InputProps
+	extends Omit<Omit<React.InputHTMLAttributes<HTMLInputElement>, "onChange">, "width"> {
+	title?: string;
+	onChange?: (value: string) => void;
+	width?: string;
+}
+
+export default function Input({ title, onChange, width, ...options }: InputProps) {
+	const id = useId();
+	const inputRef = useRef<HTMLInputElement>(null);
+	const buttonRef = useRef<HTMLButtonElement>(null);
+
+	const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		if (onChange) onChange(event.target.value);
+		if (buttonRef.current) {
+			buttonRef.current.hidden = event.target.value === "";
+		}
+	};
+
+	const handleClear = () => {
+		if (inputRef.current && buttonRef.current) {
+			inputRef.current.value = "";
+			buttonRef.current.hidden = true;
+		}
+	};
+
+	return (
+		<div
+			className="group relative flex select-none items-center gap-2 border-b border-slate-200 text-center focus-within:border-indigo-200"
+			style={{ width: width || "max-content" }}
+		>
+			{title && (
+				<label
+					htmlFor={id}
+					className="pointer-events-none ml-4 w-max truncate group-focus-within:text-indigo-200"
+				>
+					{title}:
+				</label>
+			)}
+			<input
+				id={id}
+				className="peer min-w-32 bg-transparent py-2 pl-3 pr-9 outline-none focus:outline-none"
+				ref={inputRef}
+				onChange={handleChange}
+				minLength={1}
+				{...options}
+			/>
+			<button
+				className="absolute right-3 rounded-full p-1 text-white transition-all hover:bg-neutral-700 active:bg-neutral-600 group-focus-within:text-indigo-200"
+				onClick={handleClear}
+				type="button"
+				ref={buttonRef}
+			>
+				<FaXmark />
+			</button>
+		</div>
+	);
+}
