47e8a7ba0b63f411f9d63fa10ef3d4b5b0bed146
Renan G Dutra <r.granatodutra@gmail.com>
2025-04-25 11:06:57 -0300

    fix: replace onClose with closeModal from context in QRModal

--- src/app/(private)/[instance]/(main)/qr-modal.tsx
+++ src/app/(private)/[instance]/(main)/qr-modal.tsx
@@ -2,6 +2,8 @@
 import { Button } from "@mui/material";
 import { QRCodeSVG } from "qrcode.react";
 import CloseIcon from "@mui/icons-material/Close";
+import { useContext } from "react";
+import { AppContext } from "../app-context";
 
 interface QRModalProps {
   qr: string;
@@ -10,15 +12,17 @@ interface QRModalProps {
 }
 
 export default function QRModal({ qr, phone, onClose }: QRModalProps) {
+  const { closeModal } = useContext(AppContext);
+
   return (
-    <dialog className="flex h-max w-max flex-col items-center gap-2 rounded-md bg-slate-800 px-6 py-4">
+    <div className="flex h-max w-max flex-col items-center gap-2 rounded-md bg-slate-800 px-6 py-4">
       <div className="flex w-full items-center justify-between">
         <h1 className="text-lg"> QrCode: {phone} </h1>
-        <Button onClick={onClose}>
+        <Button onClick={closeModal}>
           <CloseIcon sx={{ pointerEvents: "none" }} />
         </Button>
       </div>
-      <QRCodeSVG value={qr} height={320} width={320} />
-    </dialog>
+      <QRCodeSVG value={qr} height={480} width={480} />
+    </div>
   );
 }
