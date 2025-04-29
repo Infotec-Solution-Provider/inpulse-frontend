7844584cf5b7f129695146d363043c95e3f50420
Renan G Dutra <r.granatodutra@gmail.com>
2025-04-29 05:46:56 -0300

    feat: adicionar contexto para gerenciamento de grupos internos

---
+++ src/app/(private)/[instance]/(cruds)/internal-groups/internal-groups-context.tsx
@@ -0,0 +1,79 @@
+import { InternalChat, InternalGroup } from "@in.pulse-crm/sdk";
+import { createContext, ReactNode, useContext, useEffect, useState } from "react";
+import { InternalChatContext } from "../../internal-context";
+import { AuthContext } from "@/app/auth-context";
+
+interface IInternalGroupsProviderProps {
+  children: ReactNode;
+}
+
+interface IInternalGroupsContext {
+  internalGroups: Array<InternalGroup>;
+  createInternalGroup: (data: { name: string; participants: number[] }) => Promise<void>;
+  updateInternalGroup: (
+    id: number,
+    data: { name: string; participants: number[] },
+  ) => Promise<void>;
+}
+
+const InternalGroupsContext = createContext({} as IInternalGroupsContext);
+export const useInternalGroupsContext = () => {
+  const context = useContext(InternalGroupsContext);
+  if (!context) {
+    throw new Error("useWhatsappContext must be used within a WhatsappProvider");
+  }
+  return context;
+};
+
+export default function InternalGroupsProvider({ children }: IInternalGroupsProviderProps) {
+  const { token } = useContext(AuthContext);
+  const { internalApi } = useContext(InternalChatContext);
+  const [internalGroups, setInternalGroups] = useState<Array<InternalGroup>>([]);
+
+  const updateInternalGroup = async (
+    id: number,
+    data: { name: string; participants: number[] },
+  ) => {
+    if (internalApi.current) {
+      const res = await internalApi.current.updateInternalGroup(id, data);
+
+      setInternalGroups((prev) =>
+        prev.map((group) => {
+          if (group.id === id) {
+            return res;
+          }
+          return group;
+        }),
+      );
+    }
+  };
+
+  const createInternalGroup = async (data: { name: string; participants: number[] }) => {
+    if (internalApi.current) {
+      const created = await internalApi.current.createInternalChat(
+        data.participants,
+        true,
+        data.name,
+      );
+
+      setInternalGroups((prev) => [created as InternalGroup, ...prev]);
+    }
+  };
+
+  useEffect(() => {
+    if (token && internalApi.current) {
+      internalApi.current.setAuth(token);
+      internalApi.current.getInternalGroups().then((groups) => {
+        setInternalGroups(groups);
+      });
+    }
+  }, [token]);
+
+  return (
+    <InternalGroupsContext.Provider
+      value={{ internalGroups, updateInternalGroup, createInternalGroup }}
+    >
+      {children}
+    </InternalGroupsContext.Provider>
+  );
+}
