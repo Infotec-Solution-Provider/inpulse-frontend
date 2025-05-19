97ba5fbf829a9205a0e901bee4ee8b4eec87c4dd
juniomatias <jose.junio@bkopen.com>
2025-05-19 00:37:54 -0300

    fix: lista vazia

--- src/app/(private)/[instance]/(cruds)/ready-messages/ready-messages-context.tsx
+++ src/app/(private)/[instance]/(cruds)/ready-messages/ready-messages-context.tsx
@@ -95,8 +95,7 @@ function replaceVariables({
           file,
           TITULO, TEXTO_MENSAGEM
         );
-        if(created)
-         setReadyMessages((prev) => [created, ...(prev || [])]);
+        setReadyMessages((prev) => [created, ...(prev || [])]);
       } catch (error) {
         console.error("Error creating readyMessage", error);
         toast.error("Erro ao criar Mensagem pronta");
