aa4070de7279954e784ca9858d90de0e755e6a1e
Renan G. Dutra <r.granatodutra@gmail.com>
2025-10-15 13:02:04 -0300

    refactor(contacts): fix formatting and simplify layout structure in ContactsManagementPage

--- src/app/(private)/[instance]/(cruds)/contacts/page.tsx
+++ src/app/(private)/[instance]/(cruds)/contacts/page.tsx
@@ -1,10 +1,10 @@
-"use client"
+"use client";
 import ContactsTable from "./(table)/table";
 
 export default function ContactsManagementPage() {
   return (
-    <div className="flex flex-col px-10 pt-5 w-screen h-screen box-border relative bg-white text-black dark:bg-gray-900 dark:text-white">
-        <ContactsTable />
-      </div>
+    <div className="box-border flex h-screen w-screen flex-col px-10 pt-5">
+      <ContactsTable />
+    </div>
   );
 }
